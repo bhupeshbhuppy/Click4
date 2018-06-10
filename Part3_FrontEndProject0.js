31ca3b98ef9b5d4736063e3e277caa1806a1052a
Bhupesh Joshi <bhupesh.bhuppy@gmail.com>
2018-06-11 02:41:51 +0530

    Added Horizontal win
    To add vertical win and diagonal win

--- Part3_FrontEndProject0.js
+++ Part3_FrontEndProject0.js
@@ -6,31 +6,59 @@ var numColumn = $.map($('button'), function(n, i) { return i; }).length/numRows;
 var chance = true;
 var rowtofill = [numRows-1,numRows-1,numRows-1,numRows-1,numRows-1,numRows-1,numRows-1];
 //var rowtofill = [5,5,5,5,5,5,5];
-var rowsWithBlue = [];
-var rowsWithRed = [];
-var colWithBlue = [];
-var colwithRed = [];
 $('h3').text(player1 + ' : it is your turn, please pick a column to drop your blue chip.');
 $('td').click(function() {
-    var myCol = $(this).index();
+    var myCol = parseInt($(this).index());
     var $tr = $(this).closest('tr');
     var myRow = $tr.index();
+    console.log(myCol +" + " +myRow);
+    console.log(myCol+myRow*numColumn)
     if(chance == true){
       $('h3').text(player2 + ' : it is your turn, please pick a column to drop your red chip.');
       $('button').eq(myCol+rowtofill[myCol]*numColumn).css({backgroundColor: "Blue"});
-      rowsWithBlue.push(rowtofill[myCol]);
-      colWithBlue.push(myCol);
+      checkHorizontalWin();
       rowtofill[myCol]--
       chance = false;
       }
     else {
       $('h3').text(player1 + ' : it is your turn, please pick a column to drop your blue chip.');
       $('button').eq(myCol+rowtofill[myCol]*numColumn).css({backgroundColor: "Red"});
-      rowsWithRed.push(rowtofill[myCol]);
-      colwithRed.push(myCol);
+      checkHorizontalWin();
       rowtofill[myCol]--
       chance = true;
     }
-    console.log(rowsWithRed);
-    console.log(rowsWithBlue);
   });
+
+  function checkHorizontalWin(){
+    for(i = 0 ; i<numRows;i++){
+      for(j=0;j<numColumn-3; j++){
+        var one = getBGColor(i,j)
+        var two = getBGColor(i,j+1)
+        var three = getBGColor(i,j+2)
+        var four = getBGColor(i,j+3)
+        if(one===two && one===three && one===four && one !== 'rgb(128, 128, 128)' && one !== undefined){
+          if(chance == true){
+            $('h3').fadeOut('fast');
+            $('h2').fadeOut('fast');
+            $('h1').text(player1+" has won! Refresh your browser to play again!").css("fontSize", "50px")
+          }
+          else {
+            $('h3').fadeOut('fast');
+            $('h2').fadeOut('fast');
+            $('h1').text(player2+" has won! Refresh your browser to play again!").css("fontSize", "50px")
+          }
+        }
+      }
+    }
+  }
+function checkVerticalWin(){
+  // TO do
+}
+
+function checkDiagonalWin(){
+  // To do
+}
+
+function getBGColor(rowIndex,colIndex){
+    return $('button').eq(colIndex+rowIndex*numColumn).css('background-color');
+  }
